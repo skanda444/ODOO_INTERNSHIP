bef4b755bc999b3719a65835525685598c3de25c
skanda444 <skandachandir@gmail.com>
2025-04-09 16:44:40 +0530

    Updated

--- ZestyBeanz/models/model_one.py
+++ ZestyBeanz/models/model_one.py
@@ -13,3 +13,13 @@ class ModelOne(models.Model):
     joining_date = fields.Date(string="Joining Date", required=True)
     partner_ids = fields.Many2many('res.partner',string="Partner")
     sale_ids = fields.Many2many('sale.order','model_one_sale_rel','model_one_id','sale_id', string="Sale")
+    model_one_line_ids = fields.One2many('model.one.lines', 'model_one_id', string="Product")
+
+class ModelOnelines(models.Model):
+    _name = "model.one.lines"
+    _description = "Model One lines"
+
+    name = fields.Char(string="Name", help='A normal name field')
+    price = fields.Float(string="Standard Price")
+    product_id = fields.Many2one('product.template', string="Product")
+    model_one_id = fields.Many2one('model.one', string="Model One")
