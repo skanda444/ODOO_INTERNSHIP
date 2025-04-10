fd81b9ea3cd18dccf5606ce1180958e2095785b1
skanda444 <skandachandir@gmail.com>
2025-04-10 18:22:36 +0530

    custom sequence added

--- ZestyBeanz/models/model_one.py
+++ ZestyBeanz/models/model_one.py
@@ -1,9 +1,10 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ModelOne(models.Model):
     _name = "model.one"
     _description = "Model One"
 
+    seq=fields.Char(string="Sequence")
     name = fields.Char(string="Name", help='A normal name field', required=True, copy=False)
     age = fields.Integer(string="Age", default=18)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string="Gender", required=True, copy=False)
@@ -15,6 +16,13 @@ class ModelOne(models.Model):
     sale_ids = fields.Many2many('sale.order','model_one_sale_rel','model_one_id','sale_id', string="Sale")
     model_one_line_ids = fields.One2many('model.one.lines', 'model_one_id', string="Product")
 
+    @api.model
+    def create(self,vals):
+        print('----------', self.env['ir.sequence'])
+        vals['seq'] = self.env['ir.sequence'].next_by_code('sequence.model.one')
+        res = super(ModelOne,self).create(vals)
+        return res
+
 class ModelOnelines(models.Model):
     _name = "model.one.lines"
     _description = "Model One lines"
@@ -22,4 +30,4 @@ class ModelOnelines(models.Model):
     name = fields.Char(string="Name", help='A normal name field')
     price = fields.Float(string="Standard Price")
     product_id = fields.Many2one('product.template', string="Product")
-    model_one_id = fields.Many2one('model.one', string="Model One")
+    model_one_id = fields.Many2one('model.one', string="Model One", domain="['|',('gender', '=', 'female'),('age','>',18)]")
