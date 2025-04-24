6228dd880aeba406cfc8a4ed09b59b5486cf502d
skanda444 <skandachandir@gmail.com>
2025-04-24 15:48:49 +0530

    All Updated

--- ZestyBeanz/models/model_one.py
+++ ZestyBeanz/models/model_one.py
@@ -15,9 +15,34 @@ class ModelOne(models.Model):
 
     partner_ids = fields.Many2many('res.partner',string="Partner")
     sale_ids = fields.Many2many('sale.order','model_one_sale_rel','model_one_id','sale_id', string="Sale")
+    product_ids = fields.Many2many('product.template', 'model_one_prduct_rel', 'model_one_id', 'product_id',  string="Products")
     model_one_line_ids = fields.One2many('model.one.lines', 'model_one_id', string="Product")
     sale_id = fields.Many2one('sale.order', string="Sales")
 
+    def write_values(self):
+        #many2many fields
+        products = self.env['product.template'].search([('list_price', '>', 200)], limit=1).id
+        order = self.env['sale.order'].search([('id', '=', 26)], limit=1).id
+        #self.write({'product_ids' : [[6, 0, products]]}) 
+        #self.write({'product_ids' : [[5]]})
+        #self.write({'product_ids' : [[4, products]]})
+        #self.write({'product_ids' : [[3, products]]})
+        #self.write({'sale_ids' : [[2, order]]})
+        #self.write({'product_ids' : [[0, 0, {'name': 'Test Product', 'list_price':200}]]})
+
+        #one2many fields
+        #self.write({'model_one_line_ids' : [[0, 0, {'name': 'Test 1', 'product_id':products, 'price': 250}]]})
+        #line = self.model_one_line_ids.filtered(lambda l : l.price == 300).id
+        #existing_line = self.env['model.one.lines'].search([('price', '=', 300)], limit=1).id
+        ex_line = self.env['model.one.lines'].search([('model_one_id', '=', False)], limit=1).id
+        #self.write({'model_one_line_ids' : [[1, line, {'price': 350}]]})
+        #self.write({'model_one_line_ids' : [[2, line]]})
+        #self.write({'model_one_line_ids' : [[3, line]]})
+        #self.write({'model_one_line_ids' : [[4, existing_line]]})
+        #self.write({'model_one_line_ids' : [[4, ex_line]]})
+        self.write({'model_one_line_ids' : [[6, 0, ex_line]]})
+        
+    
     def helloworld(self):
         print("Hello World")
     
